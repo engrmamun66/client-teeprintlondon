d0dc04fcda6e694f03b3fafea74bc98908dfb461
Al-Mamun <mamun@leapinglogic.com>
2025-01-13 21:42:01 +0600

    Update

--- pages/index.vue
+++ pages/index.vue
@@ -446,7 +446,7 @@ Visit Design in London to explore our premium design services and elevate your b
         <div class="teeprint-top-footer">
             <div class="container">
                 <div class="row">
-                    <div class="col-lg-3 col-md-3 col-sm-4">
+                    <div class="col-lg-2 col-md-2 col-sm-4">
                         <div class="teeprint-footer-location">
                             <h4>Get in touch</h4>
                             <ul>
@@ -456,7 +456,7 @@ Visit Design in London to explore our premium design services and elevate your b
                             </ul>
                         </div>
                     </div>
-                    <div class="col-lg-3 col-md-3 col-sm-4">
+                    <div class="col-lg-2 col-md-2 col-sm-4">
                         <div class="teeprint-footer-links">
                             <h4>Company</h4>
                             <ul>
@@ -467,7 +467,7 @@ Visit Design in London to explore our premium design services and elevate your b
                             </ul>
                         </div>
                     </div>
-                    <div class="col-lg-3 col-md-3 col-sm-4">
+                    <div class="col-lg-2 col-md-2 col-sm-4">
                         <div class="teeprint-footer-links">
                             <h4>Discover</h4>
                             <ul>
@@ -490,6 +490,16 @@ Visit Design in London to explore our premium design services and elevate your b
                             <h4>Payment</h4>
                             <img src="https://s3.us-east-2.amazonaws.com/images.rentmy.co/content-image/3534/media/1728922614_6uosh0lb_payment.png">
                         </div>
+                    </div>
+                    <div class="col-lg-3 col-md-3 col-sm-4">
+                      <iframe
+                          src="https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d1981.2238941316784!2d-0.063695!3d51.5145948!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x487603e63fbc2617%3A0x74202ec123e41897!2sTee%20Print%20London!5e0!3m2!1sen!2suk!4v1694444089240!5m2!1sen!2suk"
+                          height="200"
+                          style="border:0;width: 100%;"
+                          allowfullscreen=""
+                          loading="lazy"
+                          referrerpolicy="no-referrer-when-downgrade"
+                      ></iframe>
                     </div>
                 </div>
             </div>
