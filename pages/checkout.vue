0600eed29841f5cabde5aa4335f37eea9e5072c8
kamrulislam0271 <kislam@leapinglogic.com>
2025-02-02 23:04:54 +0600

    checkout page added

--- pages/checkout.vue
+++ pages/checkout.vue
@@ -1,3 +1,221 @@
 <template>
-    <p>Check Out</p>
+        <section class="teeprint-checkout-wrapper">
+        <div class="container">
+            <div class="row">
+                <div class="col-lg-6">
+                    <div class="teeprint-billing-fullfilment-content">
+                        <div class="teeprint-billing-address">
+                            <h2 class="teeprint-checkout-title">Billing details </h2>
+                            <div class="row">
+                                <div class="col-lg-6">
+                                    <div class="form-group">
+                                        <label>First Name</label>
+                                        <input type="text" />
+                                    </div>
+                                </div>
+                                <div class="col-lg-6">
+                                    <div class="form-group">
+                                        <label>Last Name</label>
+                                        <input type="text" />
+                                    </div>
+                                </div>
+                                <div class="col-lg-6">
+                                    <div class="form-group">
+                                        <label>Mobile Number*</label>
+                                        <input type="text" />
+                                    </div>
+                                </div>
+                                <div class="col-lg-6">
+                                    <div class="form-group">
+                                        <label>Email Name*</label>
+                                        <input type="email" />
+                                    </div>
+                                </div>
+                                <div class="col-lg-12">
+                                    <div class="form-group">
+                                        <label>Country / Region </label>
+                                        <select>
+                                            <option>United America</option>
+                                            <option>Bangladesh</option>
+                                            <option>India</option>
+                                        </select>
+                                    </div>
+                                </div>
+                                <div class="col-lg-6">
+                                    <div class="form-group">
+                                        <label>City</label>
+                                        <input type="text" />
+                                    </div>
+                                </div>
+                                <div class="col-lg-6">
+                                    <div class="form-group">
+                                        <label>Zipcode</label>
+                                        <input type="text" />
+                                    </div>
+                                </div>
+                                <div class="col-lg-6">
+                                    <div class="form-group">
+                                        <label>State</label>
+                                        <input type="text" />
+                                    </div>
+                                </div>
+                            </div>
+                        </div>
+                    </div>
+                    <div class="teeprint-billing-fullfilment-content">
+                        <div class="teeprint-billing-address">
+                            <label class="teeprint-checkbox teeprint-checkout-title">
+                                <input type="checkbox" />
+                                Shipp to a different address
+                                <span></span>
+                            </label>
+                            <!-- <h2 class="teeprint-checkout-title">Shipp to a different address</h2> -->
+                             <div class="shipp-address">
+                                <div class="row" style="display: none;">
+                                    <div class="col-lg-6">
+                                        <div class="form-group">
+                                            <label>First Name</label>
+                                            <input type="text" />
+                                        </div>
+                                    </div>
+                                    <div class="col-lg-6">
+                                        <div class="form-group">
+                                            <label>Last Name</label>
+                                            <input type="text" />
+                                        </div>
+                                    </div>
+                                    <div class="col-lg-6">
+                                        <div class="form-group">
+                                            <label>Mobile Number*</label>
+                                            <input type="text" />
+                                        </div>
+                                    </div>
+                                    <div class="col-lg-6">
+                                        <div class="form-group">
+                                            <label>Email Name*</label>
+                                            <input type="email" />
+                                        </div>
+                                    </div>
+                                    <div class="col-lg-12">
+                                        <div class="form-group">
+                                            <label>Country / Region </label>
+                                            <select>
+                                                <option>United America</option>
+                                                <option>Bangladesh</option>
+                                                <option>India</option>
+                                            </select>
+                                        </div>
+                                    </div>
+                                    <div class="col-lg-6">
+                                        <div class="form-group">
+                                            <label>City</label>
+                                            <input type="text" />
+                                        </div>
+                                    </div>
+                                    <div class="col-lg-6">
+                                        <div class="form-group">
+                                            <label>Zipcode</label>
+                                            <input type="text" />
+                                        </div>
+                                    </div>
+                                    <div class="col-lg-6">
+                                        <div class="form-group">
+                                            <label>State</label>
+                                            <input type="text" />
+                                        </div>
+                                    </div>
+                                </div>
+                                <div class="row">
+                                    <div class="col-lg-12">
+                                        <label>Order notes (optional)</label>
+                                        <textarea class="form-control ordernote-textarea" rows="4" placeholder="Notes about your order, e.g. special notes for delivery."></textarea>
+                                    </div>
+                                </div>
+                             </div>
+                        </div>
+                    </div>
+                </div>
+                <div class="col-lg-6">
+                    <div class="teeprint-checkout-ordersummery">
+                        <div class="teeprint-checkout-ordersummery-inner">
+                            <h2 class="teeprint-ordersummery-title">Your order</h2>
+
+                            <div class="teeprint-ordersummery-list">
+                                <div class="teeprint-order-list-box">
+                                    <div class="teeprint-order-img">
+                                        <img src="https://teeprint.london/wp-content/uploads/elementor/thumbs/T-Shirt-Printing-London-qxo057nudah6z4iq83tw731v7ht72ia0a2um6qgv3s.jpg" />
+                                    </div>
+                                    <div class="teeprint-order-product-details">
+                                        <div class="teeprint-order-product-name">
+                                            Tank with V-Neck and Panel Detail&nbsp;
+                                            <strong class="teeprint-order-product-quantity">×&nbsp;1</strong>
+                                        </div>
+                                        <div class="teeprint-order-details-bottom">
+                                            <p class="teeprint-order-quantity">Quantity: 1</p>
+                                            <p class="teeprint-order-product-price">Price: $50.00</p>
+                                        </div>
+                                    </div>
+                                </div>
+                                <div class="teeprint-order-list-box">
+                                    <div class="teeprint-order-img">
+                                        <img src="https://teeprint.london/wp-content/uploads/elementor/thumbs/T-Shirt-Printing-London-qxo057nudah6z4iq83tw731v7ht72ia0a2um6qgv3s.jpg" />
+                                    </div>
+                                    <div class="teeprint-order-product-details">
+                                        <div class="teeprint-order-product-name">
+                                            Tank with V-Neck and Panel Detail&nbsp;
+                                            <strong class="teeprint-order-product-quantity">×&nbsp;1</strong>
+                                        </div>
+                                        <div class="teeprint-order-details-bottom">
+                                            <p class="teeprint-order-quantity">Quantity: 1</p>
+                                            <p class="teeprint-order-product-price">Price: $50.00</p>
+                                        </div>
+                                    </div>
+                                </div>
+                            </div>
+
+                            <div class="teeprint-checkout-order-table">
+                                <table>
+                                    <tbody>
+                                        <tr>
+                                            <th>Subtotal</th>
+                                            <td class="text-end">$ 10.00</td>
+                                        </tr>
+                                        <tr>
+                                            <th>Shipping Charge</th>
+                                            <td class="text-end">$ 10.00</td>
+                                        </tr>
+                                        <tr>
+                                            <th>Discount</th>
+                                            <td class="text-end">$ 10.00</td>
+                                        </tr>
+                                        <tr>
+                                            <th>Tax</th>
+                                            <td class="text-end">$ 10.00</td>
+                                        </tr>
+
+                                        <tr>
+                                            <th><strong>Total</strong></th>
+                                            <td class="text-end"><strong>$ 10.00</strong></td>
+                                        </tr>
+                                    </tbody>
+                                </table>
+                            </div>
+                            <div class="teeprint-ordersummery-button">
+                                <button type="button" class="teeprint-button teeprint-theme-btn teeprint-placeorder-btn">Proceed To Submit Your Quote</button>
+                            </div>
+                        </div>
+                    </div>
+                </div>
+            </div>
+        </div>
+    </section>
 </template>
+
+<script setup>
+
+definePageMeta({
+titleTemplate: '% :: checkout',
+name: 'checkout',
+layout: 'web',
+})
+</script>
