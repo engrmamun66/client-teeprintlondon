3bb0bff621200fcd877741baf23a45698dfe1ebd
Al-Mamun <mamun@leapinglogic.com>
2025-02-06 23:14:24 +0600

    Implemented question section

---
+++ components/web/questions-old.vue
@@ -0,0 +1,121 @@
+
+<template>
+    <section class="teeprint-questions">
+      <div class="teeprint-content-inner">
+          <div class="container">
+              <div class="row">
+                  <div class="col-md-12">
+                      <div class="section-main-heading text-center">
+                          <h4 class="section-heading-title-small">
+                              <span class="line-decoration"></span> Do you have questions?
+                          </h4>
+                          <h2 class="section-heading-title-big">
+                              We are happy to answer.
+                          </h2>
+                      </div>
+                  </div>
+              </div> 
+
+
+              <div class="row">
+                  <div class="col-lg-12 col-md-12 col-sm-12 col-12">
+                      <div class="teeprint-questions-box">
+                          <div class="teeprint-questions-box-inner">
+                              <div class="teeprint-questions-box-no">
+                                  <h3>01</h3>
+                              </div>
+                              <div class="teeprint-questions-box-content">
+                                  <h5>How Many Totes Should I Get?</h5>
+                                  <p>
+                                      We’ve set up packages based on number of bedrooms 
+                                      and the “average” mover. You have an option to add 
+                                      additional totes after you’ve chosen a package. So, 
+                                      if you’re a hoarder, you should add a couple extra 
+                                      and if you’re a minimalist, perhaps take a step down.
+                                  </p>
+                              </div>
+                          </div>
+                      </div>
+                  </div>
+                  <div class="col-lg-12 col-md-12 col-sm-12 col-12">
+                      <div class="teeprint-questions-box">
+                          <div class="teeprint-questions-box-inner">
+                              <div class="teeprint-questions-box-no">
+                                  <h3>02</h3>
+                              </div>
+                              <div class="teeprint-questions-box-content">
+                                  <h5>What Happens If I Need More Totes In The Middle Of My Move?</h5>
+                                  <p>
+                                      No problem! Reach out to us and let us know how many 
+                                      more totes that you need and we will do our best to 
+                                      accommodate in a timely manner. Every order includes 
+                                      one free delivery and pick up. If you need more than 
+                                      one delivery and pick up, a $29 fee will be applied 
+                                      for each instance. Additional deliveries and pick ups 
+                                      also depend on our availability.
+                                  </p>
+                              </div>
+                          </div>
+                      </div>
+                  </div>
+                  <div class="col-lg-12 col-md-12 col-sm-12 col-12">
+                      <div class="teeprint-questions-box">
+                          <div class="teeprint-questions-box-inner">
+                              <div class="teeprint-questions-box-no">
+                                  <h3>03</h3>
+                              </div>
+                              <div class="teeprint-questions-box-content">
+                                  <h5>Can I Keep My Totes Longer If Necessary?</h5>
+                                  <p>
+                                      Absolutely! We offer rentals up to four weeks. 
+                                      If you need longer than that, we have reduced 
+                                      rates for longer term rentals. Give us a call 
+                                      for more information on how to schedule.
+                                  </p>
+                              </div>
+                          </div>
+                      </div>
+                  </div>
+                  <div class="col-lg-12 col-md-12 col-sm-12 col-12">
+                      <div class="teeprint-questions-box">
+                          <div class="teeprint-questions-box-inner">
+                              <div class="teeprint-questions-box-no">
+                                  <h3>04</h3>
+                              </div>
+                              <div class="teeprint-questions-box-content">
+                                  <h5>What if I live in an upper apartment?</h5>
+                                  <p>
+                                      Of course, movers love ours totes too! If 
+                                      you want the movers to pack your belongings, 
+                                      let them know you already have totes. If you 
+                                      will be packing yourself, simply pay the 
+                                      movers to transport the totes.
+                                  </p>
+                              </div>
+                          </div>
+                      </div>
+                  </div>
+                  <div class="col-lg-12 col-md-12 col-sm-12 col-12">
+                      <div class="teeprint-questions-box">
+                          <div class="teeprint-questions-box-inner">
+                              <div class="teeprint-questions-box-no">
+                                  <h3>05</h3>
+                              </div>
+                              <div class="teeprint-questions-box-content">
+                                  <h5>I Have Movers Already Can I Still Use Pack’er Up?</h5>
+                                  <p>
+                                      Of course, movers love ours totes too! If you 
+                                      want the movers to pack your belongings, let 
+                                      them know you already have totes. If you will 
+                                      be packing yourself, simply pay the movers to 
+                                      transport the totes.
+                                  </p>
+                              </div>
+                          </div>
+                      </div>
+                  </div>
+              </div>
+          </div>
+      </div>
+  </section>
+</template>
---
+++ components/web/questions.vue
@@ -0,0 +1,218 @@
+
+<script setup>
+
+ 
+
+
+onMounted(()=>{
+  const items = Array.from(document.querySelectorAll('.accordion button'));
+
+  function toggleAccordion() {
+    const itemToggle = this.getAttribute('aria-expanded');
+
+    items.forEach(item => {
+      item.setAttribute('aria-expanded', 'false');
+    }) 
+
+    if (itemToggle == 'false') {
+      this.setAttribute('aria-expanded', 'true');
+    }
+  }
+
+  items.forEach((item) => item.addEventListener('click', toggleAccordion)); 
+})
+
+
+</script>
+
+<template>
+    <div class="container mb-4">
+      <div class="row mb-3">
+          <div class="col-md-12">
+              <div class="section-main-heading text-center">
+                  <h4 class="section-heading-title-small">
+                      <span class="line-decoration"></span> Do you have questions?
+                  </h4>
+                  <h2 class="section-heading-title-big">
+                      We are happy to answer.
+                  </h2>
+              </div>
+          </div>
+      </div> 
+      <div class="accordion">
+        <div class="accordion-item">
+          <button id="accordion-button-1" aria-expanded="false">
+            <span class="accordion-title">Why is the moon sometimes out during the day?</span>
+            <span class="icon" aria-hidden="true"></span>
+          </button>
+          <div class="accordion-content">
+            <p>
+              Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor
+              incididunt ut labore et dolore magna aliqua. Elementum sagittis vitae et leo duis ut.
+              Ut tortor pretium viverra suspendisse potenti.
+            </p>
+          </div>
+        </div>
+        <div class="accordion-item">
+          <button id="accordion-button-2" aria-expanded="false">
+            <span class="accordion-title">Why is the sky blue?</span>
+            <span class="icon" aria-hidden="true"></span>
+          </button>
+          <div class="accordion-content">
+            <p>
+              Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor
+              incididunt ut labore et dolore magna aliqua. Elementum sagittis vitae et leo duis ut.
+              Ut tortor pretium viverra suspendisse potenti.
+            </p>
+          </div>
+        </div>
+        <div class="accordion-item">
+          <button id="accordion-button-3" aria-expanded="false">
+            <span class="accordion-title">Will we ever discover aliens?</span>
+            <span class="icon" aria-hidden="true"></span>
+          </button>
+          <div class="accordion-content">
+            <p>
+              Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor
+              incididunt ut labore et dolore magna aliqua. Elementum sagittis vitae et leo duis ut.
+              Ut tortor pretium viverra suspendisse potenti.
+            </p>
+          </div>
+        </div>
+        <div class="accordion-item">
+          <button id="accordion-button-4" aria-expanded="false">
+            <span class="accordion-title">How much does the Earth weigh?</span>
+            <span class="icon" aria-hidden="true"></span>
+          </button>
+          <div class="accordion-content">
+            <p>
+              Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor
+              incididunt ut labore et dolore magna aliqua. Elementum sagittis vitae et leo duis ut.
+              Ut tortor pretium viverra suspendisse potenti.
+            </p>
+          </div>
+        </div>
+        <div class="accordion-item">
+          <button id="accordion-button-5" aria-expanded="false">
+            <span class="accordion-title">How do airplanes stay up?</span>
+            <span class="icon" aria-hidden="true"></span>
+          </button>
+          <div class="accordion-content">
+            <p>
+              Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor
+              incididunt ut labore et dolore magna aliqua. Elementum sagittis vitae et leo duis ut.
+              Ut tortor pretium viverra suspendisse potenti.
+            </p>
+          </div>
+        </div>
+      </div>
+    </div>
+</template>
+
+
+<style scoped>
+ 
+ 
+.accordion .accordion-item {
+  border-bottom: 1px solid #e5e5e5;
+  padding-left: 15px;
+  padding-right: 15px; 
+}
+
+.accordion .accordion-item button[aria-expanded='true'] {
+  border-bottom: 1px solid #03b5d2;
+}
+
+.accordion button {
+  position: relative;
+  display: block;
+  text-align: left;
+  width: 100%;
+  padding: 1em 0;
+  color: #7288a2;
+  font-size: 1.15rem;
+  font-weight: 400;
+  border: none;
+  background: none;
+  outline: none;
+}
+
+.accordion button:hover,
+.accordion button:focus {
+  cursor: pointer;
+  color: #03b5d2;
+}
+
+.accordion button:hover::after,
+.accordion button:focus::after {
+  cursor: pointer;
+  color: #03b5d2;
+  border: 1px solid #03b5d2;
+}
+
+.accordion button .accordion-title {
+  padding: 1em 1.5em 1em 0;
+}
+
+.accordion button .icon {
+  display: inline-block;
+  position: absolute;
+  top: 18px;
+  right: 0;
+  width: 22px;
+  height: 22px;
+  border: 1px solid;
+  border-radius: 22px;
+}
+
+.accordion button .icon::before {
+  display: block;
+  position: absolute;
+  content: '';
+  top: 9px;
+  left: 5px;
+  width: 10px;
+  height: 2px;
+  background: currentColor;
+}
+.accordion button .icon::after {
+  display: block;
+  position: absolute;
+  content: '';
+  top: 5px;
+  left: 9px;
+  width: 2px;
+  height: 10px;
+  background: currentColor;
+}
+
+.accordion button[aria-expanded='true'] {
+  color: #03b5d2;
+}
+.accordion button[aria-expanded='true'] .icon::after {
+  width: 0;
+}
+.accordion button[aria-expanded='true'] + .accordion-content {
+  opacity: 1;
+  max-height: 9em;
+  transition: all 200ms linear;
+  will-change: opacity, max-height;
+}
+.accordion .accordion-content {
+  opacity: 0;
+  max-height: 0;
+  overflow: hidden;
+  transition: opacity 200ms linear, max-height 200ms linear;
+  will-change: opacity, max-height;
+}
+.accordion .accordion-content p {
+  font-size: 1rem;
+  font-weight: 300;
+  margin: 2em 0;
+}
+
+
+
+
+
+</style>
