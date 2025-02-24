20e5f8dae1f454ea3264fdc8c516b5539fcabedd
Neaz Morshed <neaz.morshed@leapinglogic.com>
2025-02-25 00:55:41 +0600

    google review updated

--- components/web/GoogleReview copy.vue
+++ components/web/GoogleReview copy.vue
@@ -1,9 +1,6 @@
 <script setup>
-/**
- * Use Example
- * ===========
- * <lazy-web-GoogleReview></lazy-web-GoogleReview> 
- */
+import { ref, onMounted } from 'vue';
+
 const widgetId = "d0b99e741d5c5237cc365589e66";
 const isLoaded = ref(false);
 let goggleReviewArea = ref(null);
@@ -39,41 +36,37 @@ const initTrustIndex = () => {
   if (typeof Trustindex !== "undefined" && Trustindex.initWidgetsFromDom) {
     Trustindex.initWidgetsFromDom();
     console.log("✅ TrustIndex widget initialized.");
-    isLoaded.value = true; 
-
+    isLoaded.value = true;
   } else {
     console.error("❌ TrustIndex failed to load. Retrying...");
     setTimeout(initTrustIndex, 2000);
   }
 };
 
-
-
 onMounted(async () => {
-  await loadTrustIndexScript();  
+  try {
+    await loadTrustIndexScript();
+    initTrustIndex();
+  } catch (error) {
+    console.error("❌ Error loading TrustIndex:", error);
+  }
 });
-
-
 </script>
 
 <template>
-    <div v-bind="$attrs">
-        <!-- <h2 class="text-center mb-2">Google Reviews</h2> -->
-        <div goggleReviewArea ref="goggleReviewArea" ></div>
-        <p class="text-center my-5 d-none">Loading reviews...</p>
-    </div>
-
+  <div v-bind="$attrs">
+    <!-- <h2 class="text-center mb-2">Google Reviews</h2> -->
+    <div ref="goggleReviewArea"></div>
+    <p v-if="!isLoaded" class="text-center my-5">Loading reviews...</p>
+  </div>
 </template>
 
-<style scoped> 
-
-[goggleReviewArea] > div:empty ~ p{
-    display: block !important;
+<style scoped>
+[goggleReviewArea] > div:empty ~ p {
+  display: block !important;
 }
-
 
 h2 {
   color: #333;
 }
-
 </style>
