058c3d54082fc048d1dbc607e6a2e95e40f2a18c
Neaz Morshed <neaz.morshed@leapinglogic.com>
2025-03-15 20:54:12 +0600

    updated code

--- utils/globalData.js
+++ utils/globalData.js
@@ -85,7 +85,7 @@ export let globalData = {
         'Polo Shirts':'/cloth/polo_shirt',
         'School and university shirts' : '/cloth/university_shirt',
         'Basketball Team Shirts' : '/cloth/basketball_shirt',
-        'Football Team shirts': '/cloth/football_shirt',
+        'Football Team Tank': '/cloth/football_shirt',
         'Sports Shirts': '/cloth/sports_shirt',
         'Promotion Shirts': '/cloth/promotion_shirts'
 
