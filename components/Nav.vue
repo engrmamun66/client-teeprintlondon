f9e304b62b88a702e6c84def3e859d1976ca33f3
Neaz Morshed <neaz.morshed@leapinglogic.com>
2026-01-08 03:53:57 +0600

    code updated

--- components/Nav.vue
+++
@@ -1,9 +0,0 @@
-<template>
-  <nuxt-link class="btn btn-outline-dark" to="/one">One</nuxt-link>
-  <nuxt-link class="btn btn-outline-dark" to="/two">Two</nuxt-link>
-  <hr>
-</template>
-
-<script setup>
-
-</script>
